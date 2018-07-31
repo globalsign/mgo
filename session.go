04bc4968bbbbf43c2756afde2f3b1fe563b48ffa
Tim Vaillancourt <tim@timvaillancourt.com>
2018-08-01 00:43:11 +0200

    Fix comment

--- session.go
+++ session.go
@@ -1227,8 +1227,8 @@ type User struct {
 	// addresses and CIDR ranges from which the user is allowed to connect
 	// to the server or from which the server can accept users.
 	//
-	// WARNING Authentication Restrictions is only supported in version 3.6
-	// and above.
+	// WARNING: Authentication Restrictions is only supported in version
+	// 3.6 and above.
 	AuthenticationRestrictions []AuthenticationRestriction `bson:"authenticationRestrictions,omitempty"`
 }
 
